90a4908080e0b1cd72c5d595029753e600dba08f
JaDavid9 <87532743+JaDavid9@users.noreply.github.com>
2024-04-08 12:40:53 +0200

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, } from 'react';
 import axios from 'axios';
 import { Link, useNavigate  } from 'react-router-dom';
 
 
 function Home() {
     const [data,setData] = useState([])
+    const [searchTerm, setSearchTerm] = useState('');
+    const [sortDirection, setSortDirection] = useState('asc');
     const navigate = useNavigate();
     useEffect(() => {
         
@@ -26,7 +28,29 @@ function Home() {
         }
 
     }
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const filteredData = data.filter(contact => {
+        const fullName = `${contact.ime} ${contact.prezime}`.toLowerCase();
+        return fullName.includes(searchTerm.toLowerCase());
+    });
+
+    const sortedData = filteredData.slice().sort((a, b) => {
+        const nameA = a.ime.toLowerCase();
+        const nameB = b.ime.toLowerCase();
+        if (sortDirection === 'asc') {
+            return nameA.localeCompare(nameB);
+        } else {
+            return nameB.localeCompare(nameA);
+        }
+    });
     
+    const toggleSortDirection = () => {
+        const newSortDirection = sortDirection === 'asc' ? 'desc' : 'asc';
+        setSortDirection(newSortDirection);
+    };
 
 
     return (
@@ -35,7 +59,20 @@ function Home() {
             <div className='w-75 rounded bg-white border shadow p-4'>
                 <div className='d-flex justify-content-end'>
                 <Link to= '/create' className='btn btn-success'>Dodaj Kontakt </Link>
+                
                 </div>
+                <div>
+                        <input
+                            type='text'
+                            placeholder='Pretraži po imenu ili prezimenu'
+                            value={searchTerm}
+                            onChange={handleSearch}
+                            className='form-control mr-2'
+                        />
+                        <button onClick={toggleSortDirection} className='btn btn-secondary'>
+                            {sortDirection === 'asc' ? 'Sortiraj ↓' : 'Sortiraj ↑'}
+                        </button>
+                    </div>
             <table className='table table-striped'>
             <thead className='thead-dark'>
                         <tr>
@@ -49,7 +86,7 @@ function Home() {
                     </thead>
                     <tbody>
           {
-            data.map((d,i) => (
+            sortedData.map((d,i) => (
               <tr key = {i}>
            
               <th>{d.ime}</th>
